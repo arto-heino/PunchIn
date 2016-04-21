0ccd4dacca1627e8ba2278d8523d297d13a29216
Arto Heino <Artsi@Arto-MacBook-Pro.local>
2016-04-21 09:59:19 +0300

    Fixed routes

--- server/routes/main_info.js
+++ server/routes/main_info.js
@@ -7,8 +7,10 @@ var Lessons = models.Lessons;
 
 exports.getRoom = function(req, res){
     var major = req.params.id;
+    res.setHeader('Content-Type', 'application/json');
 
     Main.findOne({ where: {beacon_id: major}, include: [Lessons] }).then(function(classroom) {
-        res.send(classroom);
+        console.log(classroom.lessons[0].start_time);
+        res.json(classroom);
     });
 };
